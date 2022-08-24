cde497f92fd25e1ed648759be2ad12577d9b4719
giriftzen <enes.seveng37@gmail.com>
2022-08-24 11:16:15 +0300

    [CREATE] Customer and Base Entity

---
+++ src/main/java/com/example/customer/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.customer.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "created_date")
+    @CreationTimestamp
+    private Instant createdDate;
+
+}
---
+++ src/main/java/com/example/customer/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.example.customer.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+}
