8e10c902e433019135c592f2a4f51a29404e6814
giriftzen <enes.seveng37@gmail.com>
2022-08-24 11:29:43 +0300

    [CREATE] CustomerRepository

---
+++ src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.customer.repository;
+
+import com.example.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+}
